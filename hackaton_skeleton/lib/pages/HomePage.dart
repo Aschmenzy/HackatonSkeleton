048bb4bc8ca004fc5508fbb3f9e3c7dadce1ac18
Karlo Ciciliani <97040384+Karlo11111@users.noreply.github.com>
2023-12-13 11:12:15 +0100

    TextField

--- hackaton_skeleton/lib/pages/HomePage.dart
+++ hackaton_skeleton/lib/pages/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:hackaton_skeleton/util/my_text_field.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,8 +6,12 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String hintText = "Name";
+    final controller = TextEditingController();
+
     return Scaffold(
-      
+      body: MyTextField(
+          controller: controller, hintText: hintText, obscureText: false),
     );
   }
 }
---
+++ hackaton_skeleton/lib/util/my_text_field.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(20),
+        child: TextField(
+          controller: controller,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: BorderSide(
+                color: Colors.blue,
+                width: 2.0,
+              ),
+            ),
+            labelText: hintText,
+            labelStyle: TextStyle(
+              color: Colors.black,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+          ),
+        ));
+  }
+}
